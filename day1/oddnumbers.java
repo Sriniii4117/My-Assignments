8cb036c1b8c02393ed53280a0e7fc0d658fc93d8
Sriniii4117 <127009545+Sriniii4117@users.noreply.github.com>
2023-03-05 13:28:01 +0530

    Add files via upload

---
+++ day1/oddnumbers.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class oddnumbers {
+
+	public static void main(String[] args) {
+		int j=20; 
+		for (int i=1; i<20; i++) {
+    	   if (i%2==1)
+    		   System.out.println(i);
+       }
+	}
+
+}
